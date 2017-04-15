e9ac62d1a8fde6c2bdc5f6f5ace862a4f29cafae
Jose Briceño <josealbertino4@gmail.com>
2017-04-15 08:09:43 -0430

    Se edito los permisos con respecto a los usuarios, pendiente de mejorar los permisos en java

--- SQL-Scripts/Permisos.sql
+++ SQL-Scripts/Permisos.sql
@@ -1,8 +1,9 @@
 CREATE ROLE Coordinador NOSUPERUSER;
 CREATE ROLE Programador NOSUPERUSER;
-
-GRANT SELECT on SCHEMA Maraton-Progra to Coordinador; --Puede consultar todas las tablas
-GRANT UPDATE on SCHEMA Maraton-Progra to Coordinador; --Puede modificar todas las tablas
-GRANT DELETE on SCHEMA Maraton-Progra to Coordinador; -- Puede Borrar cualquier tabla
-
-CREATE USER Coordinador NOSUPERUSER IN ROLE Coordinador PASSWORD '12345';
+CREATE USER user1 NOSUPERUSER IN ROLE Coordinador PASSWORD '12345';
+CREATE USER user2 NOSUPERUSER IN ROLE Programador PASSWORD '12345';
+GRANT SELECT,UPDATE,DELETE ON ALL TABLES IN SCHEMA Maraton to Coordinador; --Puede consultar,modificar,y borrar todas las tablas
+GRANT INSERT,SELECT ON Maraton.Integrante TO Programador; --Aqui solo debe ver e insertar su informacion personal y solo ver el de su coach y entrenador
+GRANT INSERT,SELECT ON Maraton.Reporte TO Programador; -- Puede ver e insertar su informacion referida a los viajes
+GRANT SELECT ON Maraton.Resuelve TO Programador; --Puede ver sus problemas resueltos
+GRANT SELECT ON Maraton.Participa TO Programador;--  Puede ver su informacion sobre las competencia
