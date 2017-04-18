c8b3b36b90313733243ed994549ac6e0c7b02b08
Jose Briceño <josealbertino4@gmail.com>
2017-04-18 18:50:47 -0430

    Consulta top10

--- SQL-Scripts/Consulta2.sql
+++ SQL-Scripts/Consulta2.sql
@@ -1,5 +1,6 @@
-SELECT NombreEquipo, ROW_NUMBER() OVER(PARTITION BY NombreEquipo
-                              ORDER BY Clasificacion) AS Orden
+SELECT NombreEquipo, Orden
+FROM (SELECT NombreEquipo, ROW_NUMBER() OVER(ORDER BY Clasificacion) AS Orden
 FROM Maraton.Equipo
-ORDER BY Orden, CAST(SUBSTRING(NombreEquipo FROM 7) AS INTEGER);
+ORDER BY Orden, CAST(SUBSTRING(NombreEquipo FROM 7) AS INTEGER)) AS RA
+WHERE Orden <= 10;
 
